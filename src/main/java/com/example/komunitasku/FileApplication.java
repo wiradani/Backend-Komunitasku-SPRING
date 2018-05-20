336d78592911febf6cc92e9fb4e8dbb119d9fba1
wiradani <adhan432@gmail.com>
2018-05-20 10:35:36 +0700

    Upload FIle

---
+++ src/main/java/com/example/komunitasku/FileApplication.java
@@ -0,0 +1,19 @@
+package com.example.komunitasku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
+
+public class FileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/komunitasku/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.example.komunitasku;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
